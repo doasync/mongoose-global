64c70910fac3c66c0f05d7c01af2d0f246e55094
doasync <doasync@gmail.com>
2017-08-04 21:46:36 +0300

    master

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const mongoose = require('mongoose');
+const connectDb = require('mongoose-connect-db');
+
+function fn (dbURI, options = {}) {
+  if(dbURI && typeof dbURI !== 'string') {
+    throw new Error('Database URI should be a string');
+  }
+  dbURI && connectDb(mongoose, dbURI, options);
+  return mongoose;
+}
+
+Object.defineProperties(fn, {
+  mongoose: {value: mongoose},
+  connect: {value: fn},
+});
+
+module.exports = fn;
